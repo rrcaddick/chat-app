68a2ba9a075646b5b068a0ad356d73bea2d2445d
rrcaddick <rrcaddick@gmail.com>
2022-08-04 19:15:04 +0200

    Added message bubble triangle

--- frontend/src/components/Messages/Message.jsx
+++ frontend/src/components/Messages/Message.jsx
@@ -2,7 +2,14 @@ import { Avatar, Box, Text, Tooltip } from "@chakra-ui/react";
 
 const Message = ({ message, senderIsUser, displayAvatar, displaySender }) => {
   return (
-    <Box display="flex" justifyContent={senderIsUser ? "flex-end" : "flex-start"} alignItems="center" flexShrink={1}>
+    <Box
+      display="flex"
+      justifyContent={senderIsUser ? "flex-end" : "flex-start"}
+      marginRight={senderIsUser ? 0 : 2}
+      marginLeft={senderIsUser ? 2 : 0}
+      alignItems="center"
+      flexShrink={1}
+    >
       <Tooltip label={message.sender.name}>
         <Avatar
           src={message.sender.profilePicture}
@@ -18,8 +25,29 @@ const Message = ({ message, senderIsUser, displayAvatar, displaySender }) => {
         flexDir={senderIsUser ? "column" : "row"}
         bg={senderIsUser ? "#b9f5d0" : "#bee3f7"}
         borderRadius={10}
+        borderTopRightRadius={senderIsUser ? "0" : "10"}
+        borderTopLeftRadius={senderIsUser ? "10" : "0"}
         py={1}
         px={15}
+        mr="10px"
+        position="relative"
+        sx={{
+          _before: {
+            content: "''",
+            position: "absolute",
+            width: "0",
+            height: "0",
+            borderWidth: "5px",
+            borderColor: senderIsUser ? "#b9f5d0" : "#bee3f7",
+            borderBottomColor: "transparent",
+            borderLeftColor: !senderIsUser && "transparent",
+            borderRightColor: senderIsUser && "transparent",
+            left: !senderIsUser && "0",
+            right: senderIsUser && "0",
+            top: "0",
+            transform: senderIsUser ? "translateX(100%)" : "translateX(-100%)",
+          },
+        }}
       >
         <Box mr={senderIsUser ? 0 : 2} gap={1}>
           {displaySender && (
